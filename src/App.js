a820e3554b4a4af896f17ce61ea9bd3d3c0455bf
Manuel Franzini <manuelfranzini@hotmail.ch>
2024-11-29 21:18:38 +0100

    static db, grid, project count, cv, name as logo

--- src/App.js
+++ src/App.js
@@ -1,61 +1,120 @@
-import React, { useState } from 'react';
-import data from './staticDB';
+import React, { useState, useEffect, useRef } from 'react';
+import { projects, personalDetails } from './staticDB';
 import './App.css';
 
-
-
-const Popup = ({ dataToShow, onClose }) => {
-    return (
-        <ul class="popup">
-            <div className="popup-content">
-                {Object.entries(dataToShow).map(([key, value]) => (
-                    <li className="list-point" key={key}>
-                        <div ><strong>{key} <br/> </strong> {Array.isArray(value) ? value.join(', ') : value}</div>
-                    </li>
-                ))}
-                <span class="close-popup" onClick={onClose}>
-                    &times;
-                </span>
-            </div>
-        </ul>
-    );
-};
-
 const App = () => {
-    const [selectedData, setSelectedData] = useState(null);
-    const [showButtons, setShowButtons] = useState(true);
+    const [selectedWhat, setSelectedWhat] = useState(null);
 
-    const handleButtonClick = (index) => {
-        setSelectedData(data[index]);
-        setShowButtons(false);
-    };
+    const groupedData = projects.reduce((acc, item) => {
+        if (!acc[item.date]) {
+            acc[item.date] = []; // Initialize an empty array for a new year
+        }
+        acc[item.date].push(item); // Add the project to the array for the corresponding year
+        return acc;
+    }, {}); // Start with an empty object
 
-    const handleClosePopup = () => {
-        setSelectedData(null);
-        setShowButtons(true);
+    const handleButtonClick = (what) => {
+        setSelectedWhat(selectedWhat === what ? null : what);
     };
 
     return (
-        <div>
-            {showButtons && (
-                <div>
-                    {data.map((item, index) => (
-                        <button className="button" key={index} onClick={() => handleButtonClick(index)}>
-                            {item.What}
-                        </button>
+        <div className="container">
+
+            <div className="left-section">
+                <div className="project-count-wrapper">
+                    <div className="project-count">{projects.length}</div>
+                </div>
+
+                <div className="year-list-container">
+                    {Object.keys(groupedData)
+                        .sort((a, b) => b - a)
+                        .map((year) => (
+                            <div key={year} className="year-list">
+                                <div className="year">{year}</div>
+
+                                <div className="button-container">
+                                    {groupedData[year].map((item) => (
+                                        <div key={item.What}>
+                                            <button
+                                                className={`button ${selectedWhat === item.What ? "active" : ""}`}
+                                                onClick={() => handleButtonClick(item.What)}
+                                            >
+                                                {item.What}
+                                            </button>
+
+                                            <div className={`details ${selectedWhat === item.What ? "expand" : ""}`}>
+                                                <div >
+                                                    {Object.entries(item).map(([key, value]) => (
+                                                        key !== "What" && key !== "date" && (
+                                                            <div className="key-value-row" key={key}>
+                                                                <div className="key">{key}</div>
+                                                                <div className="value">
+                                                                    {Array.isArray(value) ? value.join(", ") : value}
+                                                                </div>
+                                                            </div>
+                                                        )
+                                                    ))}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        ))}
+                </div>
+            </div >
+
+
+
+            <div className="right-section">
+                <div className="right-cv-part scrollable">
+                    {Object.entries(personalDetails).map(([key, values]) => (
+                        <div className="cv-key-value-row" key={key}>
+                            <div className="cv-key">{key}</div>
+                            <div
+                                className={`value ${key === 'OS' ||
+                                    key === 'IDE' ||
+                                    key === 'Conception' ||
+                                    key === 'LCM' ||
+                                    key === 'Virtualization' ||
+                                    key === 'VersionControl' ||
+                                    key === 'Coding' ||
+                                    key === 'Links' ||
+                                    key === 'Contact' ||
+                                    key === 'Automation' ||
+                                    key === 'Languages'
+                                    ? 'horizontal' : 'vertical'
+                                    }`}
+                            >
+                                {Array.isArray(values) ? (
+                                    values
+                                        .filter(value => value)
+                                        .map((value, index) => (
+                                            <div className="cv-value" key={index}>
+                                                {value}
+                                            </div>
+                                        ))
+                                ) : (
+                                    <div className="cv-value">{values}</div>
+                                )}
+                            </div>
+                        </div>
                     ))}
                 </div>
-            )}
-            {!showButtons && (
-                <Popup dataToShow={selectedData} onClose={handleClosePopup} />
-            )}
-            <div class="background-text">Manuel Franzini</div>
+                <div className="right-name-part">
+                    <div className="name-text">Manuel <br />Franzini</div>
+                </div>
+
+            </div>
+
         </div>
+
     );
 };
-
 export default App;
 
+
+
 /*
-        
+
 */
--- src/staticDB.js
+++ src/staticDB.js
@@ -1,26 +1,217 @@
-const data = [
-    {
-        What: 'About',
-        Origin: ['CH Switzerland', 'Bern'],
-        Education: ['Bürofachdiplom VSH', 'Handelsdiplom VSH', 'Certificate of higher education in Computing & IT and Business', 'BSc Computing & IT'],
-        Certificates: ['AZ-900', 'PL-900', 'PL-100', 'PL-200'],
-        Languages: ['Swiss-German native', 'German superior', 'English professional', 'French professional', 'Spanish limited'],
+
+/*
+Format description
+Mandatory keys: Title, Date
+Use of Date: Date is an identifier to which year it shall be mapped.
+Link: Links are urls and are casted to pressable buttons.
+
+Default properties
+# remove empty properties and keep the order.
+{
+        What: '',
+        date: '202',
+        customer: '',
+        techstack: ['', ''],
+
+        description: '',
     },
+*/
+
+export const projects = [
     {
         What: 'Project 1',
         techstack: ['React', 'Node.js', 'MongoDB'],
         customer: 'ABC Inc.',
         description: 'Lorem ipsum dolor sit amet.',
-        date: '2023-01-01',
+        date: '2023',
+        xx: 'sss',
+    },
+    {
+        What: 'Project 1w',
+        techstack: ['React', 'Node.js', 'MongoDB'],
+        customer: 'ABC Inc.',
+        description: 'Lorem ipsum dolor sit amet.',
+        date: '2023',
+        xx: 'sss',
+    },
+    {
+        What: 'Project 1we',
+        techstack: ['React', 'Node.js', 'MongoDB'],
+        customer: 'ABC Inc.',
+        description: 'Lorem ipsum dolor sit amet.',
+        date: '2023',
+        xx: 'sss',
+    },
+    {
+        What: 'Project 1wer',
+        techstack: ['React', 'Node.js', 'MongoDB'],
+        customer: 'ABC Inc.',
+        description: 'Lorem ipsum dolor sit amet.',
+        date: '2023',
+        xx: 'sss',
+    },
+    {
+        What: 'Pizza minestrone con focacca di roma lala lulu malä',
+        techstack: ['React', 'Node.js', 'MongoDB'],
+        customer: 'ABC Inc.',
+        description: 'Lorem ipsum dolor sit amet.',
+        date: '2023',
+        xx: 'sss',
     },
     {
         What: 'Project 2',
         techstack: ['Angular', 'Express', 'MySQL'],
         customer: 'XYZ Corp.',
         description: 'Consectetur adipiscing elit.',
-        date: '2023-02-01',
+        date: '2026',
+    },
+    {
+        What: 'Project 3',
+        techstack: ['React', 'Node.js', 'MongoDB'],
+        customer: 'ABC Inc.',
+        description: 'Lorem ipsum dolor sit amet.',
+        date: '2023',
+        customer: 'XYZ Corp.',
+    },
+    {
+        What: 'The parfume alchemist weagdsdgbsite',
+        techstack: ['Angular', 'Express', 'MySQL'],
+        customer: 'XYZ Corp.',
+        date: '2024',
+        custoafsdfsmer: 'XYZ Cddorp.',
+        customerd: 'XYZ Corp.',
+        customsdser: 'XYZ Corp.',
+    },
+    {
+        What: 'The parfume alchemistsdg webagdssite',
+        techstack: ['Angular', 'Express', 'MySQL'],
+        customer: 'XYZ Corp.',
+        date: '2024',
+        custoafsdfsmer: 'XYZ Cddorp.',
+        customerd: 'XYZ Corp.',
+        customsdser: 'XYZ Corp.',
+    },
+    {
+        What: 'The parfume alchemist webagasdsite',
+        techstack: ['Angular', 'Express', 'MySQL'],
+        customer: 'XYZ Corp.',
+        date: '2024',
+        custoafsdfsmer: 'XYZ Cddorp.',
+        customerd: 'XYZ Corp.',
+        customsdser: 'XYZ Corp.',
+    },
+    {
+        What: 'The parfume alchemist wegsgagsdddddbsite',
+        techstack: ['Angular', 'Express', 'MySQL'],
+        customer: 'XYZ Corp.',
+        date: '2024',
+        custoafsdfsmer: 'XYZ Cddorp.',
+        customerd: 'XYZ Corp.',
+        customsdser: 'XYZ Corp.',
+    },
+    {
+        What: 'The parfume alchemiagst weggaagagbsite',
+        techstack: ['Angular', 'Express', 'MySQL'],
+        customer: 'XYZ Corp.',
+        date: '2024',
+        custoafsdfsmer: 'XYZ Cddorp.',
+        customerd: 'XYZ Corp.',
+        customsdser: 'XYZ Corp.',
+    },
+    {
+        What: 'Theasdgsadg parfume alchemsaist website',
+        techstack: ['Angular', 'Express', 'MySQL'],
+        customer: 'XYZ Corp.',
+        date: '2024',
+        custoafsdfsmer: 'XYZ Cddorp.',
+        customerd: 'XYZ Corp.',
+        customsdser: 'XYZ Corp.',
+    },
+    {
+        What: 'The parfume alchemistdf wasdgebsite',
+        techstack: ['Angular', 'Express', 'MySQL'],
+        customer: 'XYZ Corp.',
+        date: '2024',
+        custoafsdfsmer: 'XYZ Cddorp.',
+        customerd: 'XYZ Corp.',
+        customsdser: 'XYZ Corp.',
+    },
+    {
+        What: 'The parfume alchemistasdf website',
+        techstack: ['Angular', 'Express', 'MySQL'],
+        customer: 'XYZ Corp.',
+        date: '2024',
+        custoafsdfsmer: 'XYZ Cddorp.',
+        customerd: 'XYZ Corp.',
+        customsdser: 'XYZ Corp.',
+    },
+    {
+        What: 'Project 5',
+        techstack: ['Angular', 'Express', 'MySQL'],
+        customer: 'XYZ Corp.',
+        description: 'Consectetur adipiscing elit.Consectedtur adipiscing elit.Condddddddsectetur adipConsectedtur adipiscing elit.Condddddddsectetur adipConsectedtur adipiscing elit.Condddddddsectetur adipConsectedtur adipiscing elit.Condddddddsectetur adipConsectedtur adipiscing elit.Condddddddsectetur adipConsectedtur adipiscing elit.Condddddddsectetur adip',
+        date: '2026',
+        ffs: 'XYZ Corp.',
+        custasdffsomsdfsdfer: 'XYZ Corp.',
     },
 ];
 
 
-export default data;
+
+export const personalDetails = {
+    Education: [
+        'BSc Computing & IT',
+        'CertHE Computing & IT',
+        'Handelsdiplom VSH',
+        'Bürofachdiplom VSH',
+    ],
+    Certificates: ['AZ-900', 'PL-900', 'PL-100', 'PL-200'],
+    OS: ['MacOS', 'Linux'],
+    IDE: ['Eclipse', 'Intellij'],
+    LCM: ['ITIL', 'Scrum', 'Agile', 'DevOps'],
+    Conception: ['UML', 'Figma'],
+    Virtualization: ['Kubernetes', 'Docker'],
+    Automation: ['Ansible', ''],
+    VersionControl: ['Git', ''],
+    Coding: ['Java 17', 'SQL', '', ''],
+    Languages: [
+        'English',
+        'German',
+        'Swiss',
+        'French',
+    ],
+    Links: ['linkedin', 'github', '', ''],
+    Contact: [
+        'example@example.com',
+        '+41 79 123 45 67',
+    ],
+
+    Contacdst: [
+        'example@example.com',
+        '+41 79 123 45 67',
+    ],
+    Cofantact: [
+        'example@example.com',
+        '+41 79 123 45 67',
+    ],
+    Condddtact: [
+        'example@example.com',
+        '+41 79 123 45 67',
+    ],
+    Conasdfasdftact: [
+        'example@example.com',
+        '+41 79 123 45 67',
+    ],
+    Conasdftact: [
+        'example@example.com',
+        '+41 79 123 45 67',
+    ],
+    Conffftact: [
+        'example@example.com',
+        '+41 79 123 45 67',
+    ],
+    Conafaftact: [
+        'example@example.com',
+        '+41 79 123 45 67',
+    ],
+};
